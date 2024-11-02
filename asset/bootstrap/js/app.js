b4cab030889657aebf5d1429ea1586874678e63d
Md-sahid <sahidsk6901@gmail.com>
2024-11-02 17:39:51 +0530

    updated navbar

--- asset/bootstrap/js/app.js
+++ asset/bootstrap/js/app.js
@@ -1,45 +1,62 @@
         
-        
-        
-        
-        
-        
-        let openHam = document.querySelector('#openHam');
-        let closeHam = document.querySelector('#closeHam');
-        let navigationItems = document.querySelector('#navigation-items');
-        let closeItem1=document.querySelector('.cItem1');
-        let closeItem2=document.querySelector('.cItem2');
-        let closeItem3=document.querySelector('.cItem3');
-        let closeItem4=document.querySelector('.cItem4');
-        let closeItem5=document.querySelector('.cItem5');
-        let closeItem6=document.querySelector('.cItem6');
+        // selection for html elements
+       let openHam = document.querySelector('#openHam');
+       let closeHam = document.querySelector('#closeHam');
+       let navigationItems = document.querySelector('#navigation-items');
+       let closeItem1=document.querySelector('.cItem1');
+       let closeItem2=document.querySelector('.cItem2');
+       let closeItem3=document.querySelector('.cItem3');
+       let closeItem4=document.querySelector('.cItem4');
+       let closeItem5=document.querySelector('.cItem5');
+       let closeItem6=document.querySelector('.cItem6');   
+       
+    //    adding togglr effect
+       let i=0
+       openHam.addEventListener("click",function(){
+           if(i==0){
+            openHam.innerHTML="&#10005;"
+            navigationItems.style.display="flex"
+               i=1
+           }
+           else{
+            openHam.innerHTML="&#9776;"
+            navigationItems.style.display="none"
+                i=0
+           }
+       })
+       closeItem1.addEventListener("click",function(){
+        navigationItems.style.display="none"
+        openHam.innerHTML="&#9776;"
+        i=0
+        })  
+             
 
-        const hamburgerEvent = (navigation, close, open) => {
-           navigationItems.style.display = navigation;
-           openHam.style.display = open;
-           closeHam.style.display = close;
-          
-               };
-
-         openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
-         closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-         closeItem1.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-         closeItem2.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-         closeItem3.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-         closeItem4.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-         closeItem5.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-         closeItem6.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-
-
-
-
-
-
-
-
-
-
-
+        closeItem2.addEventListener("click",function(){
+            navigationItems.style.display="none"
+            openHam.innerHTML="&#9776;"
+            i=0
+            })  
+            
+        closeItem3.addEventListener("click",function(){
+            navigationItems.style.display="none"
+            openHam.innerHTML="&#9776;"
+            i=0
+            })            
+        closeItem4.addEventListener("click",function(){
+            navigationItems.style.display="none"
+            openHam.innerHTML="&#9776;"
+            i=0
+            })  
+        closeItem5.addEventListener("click",function(){
+            navigationItems.style.display="none"
+            openHam.innerHTML="&#9776;"
+            i=0
+            })  
+        closeItem6.addEventListener("click",function(){
+            navigationItems.style.display="none"
+            openHam.innerHTML="&#9776;"
+            i=0
+            })  
 
 // You can change as needed
 const word_list = [
@@ -49,8 +66,6 @@ const word_list = [
     "Proud to be SRKian!",
     "Proud to be SRKian!"
 ];
-
-
 
 
 document.addEventListener("DOMContentLoaded", ()=> {
@@ -70,11 +85,6 @@ if (current_word_index >= word_list.length) {
 }
 }, 2000); // 2 second -> Remember, this delay should be the same as the typing animation duration.
 });
-
-
-
-
-
 
 // function for update heading text
 function updateHeadingText (current_word_index, heading_el) {
